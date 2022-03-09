a310383fd7e254eb41f87a8e0184f4b200302504
TingwenH <tingwenh@student.ubc.ca>
2022-03-09 13:46:14 +0800

    update

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/tingwenhua/Desktop/dashboard1-group-c/venv"
+set -gx VIRTUAL_ENV "/Users/tingwenhua/Desktop/Project/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
